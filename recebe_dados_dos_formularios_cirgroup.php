4abccbe708c0cef4e63d5e002c6bd9b6a6f0ecbd
Cirilo Lukanu <cirilo3@live.com.pt>
2021-05-19 17:08:39 +0100

    Updates1

---
+++ recebe_dados_dos_formularios_cirgroup.php
@@ -0,0 +1,7 @@
+<html><body>
+    <form action="receber_formularios.php" method="post">
+        
+    First nome:
+    <input type="text"name="nome"><br>
+    Last nome
+    <<input type="text" id="lname" name="lname"><br>
